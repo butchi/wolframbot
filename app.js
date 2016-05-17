07f9032703b3b8b221041f7a125a207072489ec6
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-05-17 21:39:43 +0900

    api call and show result

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var url = require('url');
 var app = express();
 var server = http.createServer(app);
 
-var _ = require('lodash');
-var cookie = require('cookie');
-var bodyParser = require('body-parser');
+var _ = require('underscore');
+var parseString = require('xml2js').parseString;
+var request = require('request');
 var readConfig = require('read-config');
 
 // var mongo_builder = require('./lib/mongo_builder');
@@ -22,10 +22,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser());
-
-config = readConfig('./config.json');
-console.log(config.appid);
 
 // mongo_builder.ready(dbName, function(db){
 //   console.log('db: ', db);
@@ -35,6 +31,27 @@ console.log(config.appid);
 //     });
 //   });
 // });
+
+config = readConfig('./config.json');
+
+var input;
+// input = 'pi';
+// input = '18782 + 18782';
+input = 'Fibonacci[10]';
+
+var url = `http://api.wolframalpha.com/v2/query?input=${encodeURIComponent(input)}&appid=${config.appid}`;
+
+request.get(url, function (err, res, body) {
+  if (!err && res.statusCode == 200) {
+    parseString(body, function (err, result) {
+      result.queryresult.pod.forEach((elm) => {
+        if(elm.$.id === 'Result') {
+          console.log(elm.subpod[0].plaintext[0]);
+        }
+      });
+    });
+  }
+});
 
 server.listen(app.get('port'), process.env.HOST, function(){
   console.log("Express server listening on port " + app.get('port'));
