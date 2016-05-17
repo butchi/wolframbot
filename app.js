22ddd43d105a0db7403e683801e64fc4d8f6f564
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-05-17 20:25:06 +0900

    read config

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ var http = require('http');
 var path = require('path');
 var url = require('url');
 
-var app = expres();
+var app = express();
 var server = http.createServer(app);
 
 var _ = require('lodash');
-var shortid = require('shortid');
-var Puid = require('puid');
 var cookie = require('cookie');
 var bodyParser = require('body-parser');
-var basicAuth = require('basic-auth-connect');
+var readConfig = require('read-config');
 
-var mongo_builder = require('./lib/mongo_builder');
+// var mongo_builder = require('./lib/mongo_builder');
 
-var cookiePuid = new Puid();
-var hiddenKeyPuid = new Puid();
-
-const host = '127.0.0.1';
-const dbName = 'nodedb';
-const dbPath = `mongodb://${host}/${dbName}`;
+// const host = '127.0.0.1';
+// const dbName = 'nodedb';
+// const dbPath = `mongodb://${host}/${dbName}`;
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -29,14 +24,17 @@ app.locals.pretty = true;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser());
 
-mongo_builder.ready(dbName, function(db){
-  console.log('db: ', db);
-  db.collection('users', (err, collection) => {
-    collection.find().toArray((err, items) => {
-      console.log('items: ', items);
-    });
-  });
-});
+config = readConfig('./config.json');
+console.log(config.appid);
+
+// mongo_builder.ready(dbName, function(db){
+//   console.log('db: ', db);
+//   db.collection('users', (err, collection) => {
+//     collection.find().toArray((err, items) => {
+//       console.log('items: ', items);
+//     });
+//   });
+// });
 
 server.listen(app.get('port'), process.env.HOST, function(){
   console.log("Express server listening on port " + app.get('port'));
